a416323544b36ba3490c9b838e6e63bcefc94ba3
TommyLemon <1184482681@qq.com>
2021-02-12 01:23:14 +0800

    MySQL：更新表，TestRecord 表新增 duration, minDuration, maxDuration 3 个字段用于记录和对比服务处理耗时

--- MySQL/single/sys_routines.sql
+++ MySQL/single/sys_routines.sql
@@ -65,4 +65,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-11 19:04:45
+-- Dump completed on 2021-02-12  1:22:04
