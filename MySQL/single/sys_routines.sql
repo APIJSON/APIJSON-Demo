2d4d3dba3e24d0769416f22fdf0114d79935f277
TommyLemon <1184482681@qq.com>
2022-04-11 01:27:36 +0800

    MySQL：更新表

--- MySQL/single/sys_routines.sql
+++ MySQL/single/sys_routines.sql
@@ -65,4 +65,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-05  1:41:30
+-- Dump completed on 2022-04-11  1:22:27
