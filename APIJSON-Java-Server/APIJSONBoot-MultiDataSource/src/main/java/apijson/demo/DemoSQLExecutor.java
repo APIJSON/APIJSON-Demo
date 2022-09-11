4fba986d3d01b68d16c1703bf9cf7e406dc78a18
TommyLemon <1184482681@qq.com>
2022-09-11 18:56:53 +0800

    Java：MultiDataSource 接口 /execute 新增支持 NebulaGraph

--- APIJSON-Java-Server/APIJSONBoot-MultiDataSource/src/main/java/apijson/demo/DemoSQLExecutor.java
+++ APIJSON-Java-Server/APIJSONBoot-MultiDataSource/src/main/java/apijson/demo/DemoSQLExecutor.java
@@ -15,10 +15,13 @@ limitations under the License.*/
 package apijson.demo;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.vesoft.nebula.jdbc.impl.NebulaDriver;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -33,83 +36,102 @@ import apijson.orm.SQLConfig;
  * @author Lemon
  */
 public class DemoSQLExecutor extends APIJSONSQLExecutor {
-	public static final String TAG = "DemoSQLExecutor";
+  public static final String TAG = "DemoSQLExecutor";
 
-	//  可重写以下方法，支持 Redis 等单机全局缓存或分布式缓存
-	//	@Override
-	//	public List<JSONObject> getCache(String sql, int type) {
-	//		return super.getCache(sql, type);
-	//	}
-	//	@Override
-	//	public synchronized void putCache(String sql, List<JSONObject> list, int type) {
-	//		super.putCache(sql, list, type);
-	//	}
-	//	@Override
-	//	public synchronized void removeCache(String sql, int type) {
-	//		super.removeCache(sql, type);
-	//	}
+  //  可重写以下方法，支持 Redis 等单机全局缓存或分布式缓存
+  //	@Override
+  //	public List<JSONObject> getCache(String sql, int type) {
+  //		return super.getCache(sql, type);
+  //	}
+  //	@Override
+  //	public synchronized void putCache(String sql, List<JSONObject> list, int type) {
+  //		super.putCache(sql, list, type);
+  //	}
+  //	@Override
+  //	public synchronized void removeCache(String sql, int type) {
+  //		super.removeCache(sql, type);
+  //	}
 
-	// 适配连接池，如果这里能拿到连接池的有效 Connection，则 SQLConfig 不需要配置 dbVersion, dbUri, dbAccount, dbPassword
-	@Override
-	public Connection getConnection(SQLConfig config) throws Exception {
-		String datasource = config.getDatasource();
-		Log.d(TAG, "getConnection  config.getDatasource() = " + datasource);
+  // 适配连接池，如果这里能拿到连接池的有效 Connection，则 SQLConfig 不需要配置 dbVersion, dbUri, dbAccount, dbPassword
+  @Override
+  public Connection getConnection(SQLConfig config) throws Exception {
+    if ("NEBULA".equals(config.getDatabase())) {  // 3.0.0 及以下要这样连接
+      String uri = config.getDBUri();
 
-		String key = datasource + "-" + config.getDatabase();
-		Connection c = connectionMap.get(key);
-		if (datasource != null && (c == null || c.isClosed())) {
-			try {
-				DataSource ds;
-				switch (datasource) {
-				case "HIKARICP":
-					ds = DemoApplication.getApplicationContext().getBean(HikariDataSource.class);
-					// 另一种方式是 DemoDataSourceConfig 初始化获取到 DataSource 后给静态变量 DATA_SOURCE_HIKARICP 赋值： ds = DemoDataSourceConfig.DATA_SOURCE_HIKARICP.getConnection();
-					break;
-				default:
-					Map<String, DruidDataSource> dsMap = DemoApplication.getApplicationContext().getBeansOfType(DruidDataSource.class);
-					// 另一种方式是 DemoDataSourceConfig 初始化获取到 DataSource 后给静态变量 DATA_SOURCE_DRUID 赋值： ds = DemoDataSourceConfig.DATA_SOURCE_DRUID.getConnection();
-					switch (datasource) {
-					case "DRUID-TEST":
-						ds = dsMap.get("druidTestDataSource");
-						break;
-					case "DRUID-ONLINE":
-						ds = dsMap.get("druidOnlineDataSource");
-						break;
-					case "DRUID":
-						ds = dsMap.get("druidDataSource");
-						break;
-					default:
-						ds = null;
-						break;
-					}
-					break;
-				}
+      int start = uri.indexOf("://");
+      String prefix = uri.substring(0, start);
 
-				connectionMap.put(key, ds == null ? null : ds.getConnection());
-			} catch (Exception e) {
-				Log.e(TAG, "getConnection   try { "
-						+ "DataSource ds = DemoApplication.getApplicationContext().getBean(DataSource.class); .."
-						+ "} catch (Exception e) = " + e.getMessage());
-			}
-		}
+      uri = uri.substring(start + "://".length());
+      int end = uri.indexOf("/");
+      String space = uri.substring(end + 1);
 
-		// 必须最后执行 super 方法，因为里面还有事务相关处理。
-		// 如果这里是 return c，则会导致 增删改 多个对象时只有第一个会 commit，即只有第一个对象成功插入数据库表
-		return super.getConnection(config);
-	}
+      Properties props = new Properties();
+      props.put("url", prefix + "://" + space);
+      props.put("graphSpace", space);
 
-	// 取消注释支持 !key 反选字段 和 字段名映射，需要先依赖插件 https://github.com/APIJSON/apijson-column
-	//	@Override
-	//	protected String getKey(SQLConfig config, ResultSet rs, ResultSetMetaData rsmd, int tablePosition, JSONObject table,
-	//			int columnIndex, Map<String, JSONObject> childMap) throws Exception {
-	//		return ColumnUtil.compatOutputKey(super.getKey(config, rs, rsmd, tablePosition, table, columnIndex, childMap), config.getTable(), config.getMethod());
-	//	}
+      NebulaDriver driver = new NebulaDriver(uri.substring(0, end));
+      return driver.connect(prefix + "://" + space, props);
+      //    return DriverManager.getConnection("jdbc:nebula://JDBC_TEST_SPACE", "root", "nebula");
+    }
 
-	// 不需要隐藏字段这个功能时，取消注释来提升性能
-	//	@Override
-	//	protected boolean isHideColumn(SQLConfig config, ResultSet rs, ResultSetMetaData rsmd, int tablePosition,
-	//			JSONObject table, int columnIndex, Map<String, JSONObject> childMap) throws SQLException {
-	//		return false;
-	//	}
+    String datasource = config.getDatasource();
+    Log.d(TAG, "getConnection  config.getDatasource() = " + datasource);
+
+    String key = datasource + "-" + config.getDatabase();
+    Connection c = connectionMap.get(key);
+    if (datasource != null && (c == null || c.isClosed())) {
+      try {
+        DataSource ds;
+        switch (datasource) {
+          case "HIKARICP":
+            ds = DemoApplication.getApplicationContext().getBean(HikariDataSource.class);
+            // 另一种方式是 DemoDataSourceConfig 初始化获取到 DataSource 后给静态变量 DATA_SOURCE_HIKARICP 赋值： ds = DemoDataSourceConfig.DATA_SOURCE_HIKARICP.getConnection();
+            break;
+          default:
+            Map<String, DruidDataSource> dsMap = DemoApplication.getApplicationContext().getBeansOfType(DruidDataSource.class);
+            // 另一种方式是 DemoDataSourceConfig 初始化获取到 DataSource 后给静态变量 DATA_SOURCE_DRUID 赋值： ds = DemoDataSourceConfig.DATA_SOURCE_DRUID.getConnection();
+            switch (datasource) {
+              case "DRUID-TEST":
+                ds = dsMap.get("druidTestDataSource");
+                break;
+              case "DRUID-ONLINE":
+                ds = dsMap.get("druidOnlineDataSource");
+                break;
+              case "DRUID":
+                ds = dsMap.get("druidDataSource");
+                break;
+              default:
+                ds = null;
+                break;
+            }
+            break;
+        }
+
+        connectionMap.put(key, ds == null ? null : ds.getConnection());
+      } catch (Exception e) {
+        Log.e(TAG, "getConnection   try { "
+          + "DataSource ds = DemoApplication.getApplicationContext().getBean(DataSource.class); .."
+          + "} catch (Exception e) = " + e.getMessage());
+      }
+    }
+
+    // 必须最后执行 super 方法，因为里面还有事务相关处理。
+    // 如果这里是 return c，则会导致 增删改 多个对象时只有第一个会 commit，即只有第一个对象成功插入数据库表
+    return super.getConnection(config);
+  }
+
+  // 取消注释支持 !key 反选字段 和 字段名映射，需要先依赖插件 https://github.com/APIJSON/apijson-column
+  //	@Override
+  //	protected String getKey(SQLConfig config, ResultSet rs, ResultSetMetaData rsmd, int tablePosition, JSONObject table,
+  //			int columnIndex, Map<String, JSONObject> childMap) throws Exception {
+  //		return ColumnUtil.compatOutputKey(super.getKey(config, rs, rsmd, tablePosition, table, columnIndex, childMap), config.getTable(), config.getMethod());
+  //	}
+
+  // 不需要隐藏字段这个功能时，取消注释来提升性能
+  //	@Override
+  //	protected boolean isHideColumn(SQLConfig config, ResultSet rs, ResultSetMetaData rsmd, int tablePosition,
+  //			JSONObject table, int columnIndex, Map<String, JSONObject> childMap) throws SQLException {
+  //		return false;
+  //	}
 
 }
