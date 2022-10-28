dbe1bc22ef8bf0c81980cf940be985e825e46789
TommyLemon <1184482681@qq.com>
2022-10-29 00:36:52 +0800

    Java：新增 APIJSONDemo-ClickHouse

---
+++ APIJSON-Java-Server/APIJSONDemo-ClickHouse/src/main/java/apijson/demo/DemoApplication.java
@@ -0,0 +1,99 @@
+/*Copyright ©2016 TommyLemon(https://github.com/TommyLemon/APIJSON)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package apijson.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import apijson.Log;
+import apijson.framework.APIJSONApplication;
+import apijson.framework.APIJSONCreator;
+import apijson.orm.SQLConfig;
+
+
+/**
+ * Demo SpringBoot Application 主应用程序启动类
+ * 右键这个类 > Run As > Java Application
+ * 具体见 SpringBoot 文档
+ * https://www.springcloud.cc/spring-boot.html#using-boot-locating-the-main-class
+ *
+ * @author Lemon
+ */
+@Configuration
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+public class DemoApplication implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+    public static final String TAG = "DemoApplication";
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoApplication.class, args);
+
+        Log.DEBUG = true;
+        APIJSONApplication.init(false);  // 4.4.0 以上需要这句来保证以上 static 代码块中给 DEFAULT_APIJSON_CREATOR 赋值会生效
+    }
+
+    // SpringBoot 2.x 自定义端口方式
+    @Override
+    public void customize(ConfigurableServletWebServerFactory server) {
+        server.setPort(8080);
+    }
+
+    // 支持 APIAuto 中 JavaScript 代码跨域请求
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+    static {
+        // 使用本项目的自定义处理类
+        APIJSONApplication.DEFAULT_APIJSON_CREATOR = new APIJSONCreator<Long>() {
+            @Override
+            public SQLConfig createSQLConfig() {
+                return new DemoSQLConfig();
+            }
+        };
+
+        try { //加载驱动程序
+            Log.d(TAG, "尝试加载 clickhouse 驱动 <<<<<<<<<<<<<<<<<<<<< ");
+            //	Class.forName("ru.yandex.clickhouse.ClickHouseDriver");
+            Class.forName("cc.blynk.clickhouse.ClickHouseDriver");
+            Log.d(TAG, "成功加载 clickhouse 驱动！>>>>>>>>>>>>>>>>>>>>> ");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.e(TAG, "加载 clickhouse 驱动失败，请检查 pom.xml 中 ru.yandex.clickhouse 版本是否存在以及可用 ！！！");
+        }
+
+    }
+
+}
---
+++ APIJSON-Java-Server/APIJSONDemo-ClickHouse/src/main/java/apijson/demo/DemoSQLConfig.java
@@ -0,0 +1,68 @@
+/*Copyright ©2016 TommyLemon(https://github.com/TommyLemon/APIJSON)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package apijson.demo;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import apijson.framework.APIJSONSQLConfig;
+
+
+/**SQL 配置
+ * TiDB 用法和 MySQL 一致
+ * 具体见详细的说明文档 C.开发说明 C-1-1.修改数据库链接  
+ * https://github.com/Tencent/APIJSON/blob/master/%E8%AF%A6%E7%BB%86%E7%9A%84%E8%AF%B4%E6%98%8E%E6%96%87%E6%A1%A3.md#c-1-1%E4%BF%AE%E6%94%B9%E6%95%B0%E6%8D%AE%E5%BA%93%E9%93%BE%E6%8E%A5
+ * @author Lemon
+ */
+public class DemoSQLConfig extends APIJSONSQLConfig {
+
+    static {
+        DEFAULT_DATABASE = DATABASE_PRESTO;  // TODO 默认数据库类型，改成你自己的
+        DEFAULT_SCHEMA = "sys";  // TODO 默认数据库名/模式，改成你自己的，默认情况是 MySQL: sys, PostgreSQL: public, SQL Server: dbo, Oracle:
+
+        // 表名和数据库不一致的，需要配置映射关系。只使用 APIJSONORM 时才需要；
+        // 如果用了 apijson-framework 且调用了 APIJSONApplication.init 则不需要
+        // (间接调用 DemoVerifier.init 方法读取数据库 Access 表来替代手动输入配置)。
+        // 但如果 Access 这张表的对外表名与数据库实际表名不一致，仍然需要这里注册。例如
+        //		TABLE_KEY_MAP.put(Access.class.getSimpleName(), "access");
+
+        //表名映射，隐藏真实表名，对安全要求很高的表可以这么做
+        TABLE_KEY_MAP.put("User", "apijson_user");
+        TABLE_KEY_MAP.put("Privacy", "apijson_privacy");
+    }
+
+    @Override
+    public String getDBVersion() {
+        return "21.7.5.29";  // TODO 改成你自己的
+    }
+
+    @JSONField(serialize = false)  // 不在日志打印 账号/密码 等敏感信息
+    @Override
+    public String getDBUri() {
+        return "jdbc:clickhouse://localhost:8123"; // TODO 改成你自己的
+    }
+
+    @JSONField(serialize = false)  // 不在日志打印 账号/密码 等敏感信息
+    @Override
+    public String getDBAccount() {
+        return "default";  // TODO 改成你自己的
+    }
+
+    @JSONField(serialize = false)  // 不在日志打印 账号/密码 等敏感信息
+    @Override
+    public String getDBPassword() {
+        return "123456";  // TODO 改成你自己的
+    }
+
+}
