34f9cd0194801d46fb1c8554ab96c8dffec5d86a
TommyLemon <tommylemon@qq.com>
2023-05-07 15:17:13 +0800

    Java：新增使用 SpringBoot 3.0+ 的 Demo

---
+++ APIJSON-Java-Server/APIJSONDemo-SpringBoot3/src/main/java/apijson/demo/DemoApplication.java
@@ -0,0 +1,69 @@
+/*Copyright ©2016 TommyLemon(https://github.com/TommyLemon/APIJSON)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package apijson.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * Demo SpringBoot Application 主应用程序启动类
+ * 右键这个类 > Run As > Java Application
+ * 具体见 SpringBoot 文档
+ * https://www.springcloud.cc/spring-boot.html#using-boot-locating-the-main-class
+ *
+ * @author Lemon
+ */
+@Configuration
+@SpringBootApplication
+@EnableConfigurationProperties
+public class DemoApplication implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(DemoApplication.class, args);
+
+//    Log.DEBUG = true;
+  }
+
+  // SpringBoot 2.x 自定义端口方式
+  @Override
+  public void customize(ConfigurableServletWebServerFactory server) {
+    server.setPort(8080);
+  }
+
+  // 支持 APIAuto 中 JavaScript 代码跨域请求
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+          .allowedOriginPatterns("*")
+          .allowedMethods("*")
+          .allowCredentials(true)
+          .maxAge(3600);
+      }
+    };
+  }
+
+
+}
---
+++ APIJSON-Java-Server/APIJSONDemo-SpringBoot3/src/main/java/apijson/demo/DemoParser.java
@@ -0,0 +1,137 @@
+/*Copyright ©2016 TommyLemon(https://github.com/TommyLemon/APIJSON)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package apijson.demo;
+
+import apijson.orm.*;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jakarta.servlet.http.HttpSession;
+
+import apijson.RequestMethod;
+
+
+/**请求解析器
+ * 具体见 https://github.com/Tencent/APIJSON/issues/38
+ * @author Lemon
+ */
+public class DemoParser extends AbstractParser<Long> {
+
+    public static final Map<String, HttpSession> KEY_MAP;
+    static {
+      KEY_MAP = new HashMap<>();
+    }
+
+    public DemoParser() {
+        super();
+    }
+    public DemoParser(RequestMethod method) {
+        super(method);
+    }
+    public DemoParser(RequestMethod method, boolean needVerify) {
+        super(method, needVerify);
+    }
+
+    private int maxQueryCount = 2000;
+//    //	可重写来设置最大查询数量
+//    @Override
+//    public int getMaxQueryCount() {
+//      return maxQueryCount;
+//    }
+//
+//    @Override
+//    public int getMaxUpdateCount() {
+//        return 2000;
+//    }
+//
+//    @Override
+//    public int getMaxObjectCount() {
+//        return getMaxUpdateCount();
+//    }
+//
+//    @Override
+//    public int getMaxSQLCount() {
+//        return getMaxUpdateCount();
+//    }
+
+
+    private HttpSession session;
+    public Parser<Long> setSession(HttpSession session) {
+        this.session = session;
+        return this;
+    }
+    public HttpSession getSession() {
+        return session;
+    }
+
+
+    @Override
+    public Parser<Long> createParser() {
+        return new DemoParser();
+    }
+
+    @Override
+    public ObjectParser createObjectParser(JSONObject request, String parentPath, SQLConfig arrayConfig
+            , boolean isSubquery, boolean isTable, boolean isArrayMainTable) throws Exception {
+        return new DemoObjectParser(getSession(), request, parentPath, arrayConfig
+                , isSubquery, isTable, isArrayMainTable).setMethod(getMethod()).setParser(this);
+    }
+
+    @Override
+    public FunctionParser createFunctionParser() {
+        return new DemoFunctionParser();
+    }
+
+    @Override
+    public SQLConfig createSQLConfig() {
+        return new DemoSQLConfig();
+    }
+
+    @Override
+    public SQLExecutor createSQLExecutor() {
+        return new DemoSQLExecutor();
+    }
+
+    @Override
+    public Verifier<Long> createVerifier() {
+        return new DemoVerifier();
+    }
+
+    private FunctionParser functionParser;
+    public FunctionParser getFunctionParser() {
+        return functionParser;
+    }
+    public Object onFunctionParse(String key, String function, String parentPath, String currentName, JSONObject currentObject, boolean containRaw) throws Exception {
+        if (functionParser == null) {
+            functionParser = createFunctionParser();
+            functionParser.setMethod(getMethod());
+            functionParser.setTag(getTag());
+            functionParser.setVersion(getVersion());
+            functionParser.setRequest(requestObject);
+        }
+        functionParser.setKey(key);
+        functionParser.setParentPath(parentPath);
+        functionParser.setCurrentName(currentName);
+        functionParser.setCurrentObject(currentObject);
+
+        return functionParser.invoke(function, currentObject, containRaw);
+    }
+
+
+    // 实现应用层与数据库共用账号密码，可用于多租户、SQLAuto 等 >>>>>>>>>>>>>>>
+
+}
