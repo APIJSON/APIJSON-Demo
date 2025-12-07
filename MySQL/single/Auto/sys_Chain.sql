4289b81440e958af830d58a0f091218203048a82
TommyLemon <1184482681@qq.com>
2025-12-08 01:06:14 +0800

    MySQL: 更新表

--- MySQL/single/Auto/sys_Chain.sql
+++ MySQL/single/Auto/sys_Chain.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for macos15 (arm64)
 --
 -- Host: apijson.cn    Database: sys
 -- ------------------------------------------------------
--- Server version	5.7.43-log
+-- Server version	8.4.5
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,31 +16,45 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Chain`
+-- Table structure for table `chain`
 --
 
-DROP TABLE IF EXISTS `Chain`;
+DROP TABLE IF EXISTS `chain`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Chain` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `userId` bigint(20) NOT NULL,
+CREATE TABLE `chain` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `userId` bigint NOT NULL,
   `host` varchar(100) DEFAULT NULL,
-  `testAccountId` bigint(20) NOT NULL DEFAULT '0',
-  `toGroupId` bigint(20) NOT NULL DEFAULT '0',
-  `groupId` bigint(20) NOT NULL,
+  `testAccountId` varchar(45) NOT NULL DEFAULT '0',
+  `toGroupId` bigint NOT NULL DEFAULT '0',
+  `groupId` bigint NOT NULL,
   `groupName` varchar(100) NOT NULL,
   `rank` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `documentId` bigint(20) NOT NULL DEFAULT '0',
+  `documentId` bigint NOT NULL DEFAULT '0',
   `documentName` varchar(100) DEFAULT NULL,
-  `randomId` bigint(20) NOT NULL DEFAULT '0',
+  `randomId` bigint NOT NULL DEFAULT '0',
   `randomName` varchar(100) DEFAULT NULL,
-  `scriptId` bigint(20) NOT NULL DEFAULT '0',
+  `scriptId` bigint NOT NULL DEFAULT '0',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tagList` json DEFAULT NULL COMMENT '标签列表',
+  `testName` varchar(45) DEFAULT NULL COMMENT '测试用户名称',
+  `testAccount` varchar(45) DEFAULT NULL COMMENT '测试用户账号，一般是手机号、邮箱等',
+  `testInfo` json DEFAULT NULL COMMENT '测试用户信息',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idnew_table_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8 COMMENT='多接口串联的场景链路用例';
+) ENGINE=InnoDB AUTO_INCREMENT=1761204465119 DEFAULT CHARSET=utf8mb3 COMMENT='多接口串联的场景链路用例';
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chain`
+--
+
+LOCK TABLES `chain` WRITE;
+/*!40000 ALTER TABLE `chain` DISABLE KEYS */;
+INSERT INTO `chain` VALUES (11,82001,NULL,'',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2024-10-27 13:46:19',0,NULL,0,NULL,0,'2024-05-02 13:12:36','[\"Moment\"]',NULL,NULL,NULL),(12,82001,NULL,'',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2024-10-27 13:46:19',1648334213866,NULL,0,NULL,0,'2024-05-02 13:15:49','[\"Moment\"]','','','{}'),(13,82001,NULL,'82002',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2024-10-27 13:46:19',1546414179257,NULL,0,NULL,0,'2024-05-02 13:16:02','[\"Moment\"]','Jan','13000082002','{\"id\": 82002, \"name\": \"Jan\", \"phone\": \"13000082002\", \"account\": \"13000082002\", \"baseUrl\": \"http://localhost:8080\", \"password\": \"123456\", \"redCount\": 0, \"remember\": false, \"blueCount\": 0, \"greenCount\": 0, \"isLoggedIn\": true, \"totalCount\": 18, \"whiteCount\": 18, \"orangeCount\": 0, \"summaryType\": \"total\"}'),(15,82001,NULL,'',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2024-10-27 13:46:19',1704192205562,NULL,0,NULL,0,'2024-05-02 13:21:29','[\"Moment\"]',NULL,NULL,NULL),(16,82001,NULL,'',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2024-10-27 13:46:19',1704192205563,NULL,0,NULL,0,'2024-05-02 13:21:54','[\"Moment\"]',NULL,NULL,NULL),(17,82001,NULL,'82003',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2024-10-27 13:46:19',1511969630372,NULL,0,NULL,0,'2024-05-02 13:22:10','[\"Moment\"]','Wechat','13000082003','{\"id\": 82003, \"name\": \"Wechat\", \"phone\": \"13000082003\", \"account\": \"13000082003\", \"baseUrl\": \"http://localhost:8080\", \"password\": \"123456\", \"redCount\": 0, \"remember\": false, \"blueCount\": 0, \"greenCount\": 0, \"isLoggedIn\": true, \"totalCount\": 0, \"whiteCount\": 0, \"orangeCount\": 0, \"summaryType\": \"total\"}'),(48,1704867025040,NULL,'',0,1742784526673,'查询动态列表','2025-03-24 02:48:46',0,NULL,0,NULL,0,'2025-03-24 02:48:46',NULL,NULL,NULL,NULL),(49,82001,NULL,'',0,1742784526673,'查询动态列表','2025-03-24 06:05:56',1704192205420,NULL,0,NULL,0,'2025-03-24 06:05:56','[\"Moment\", \"Tommy\"]','','','{}'),(54,1681451084123,NULL,'',0,1751529769988,'查询','2025-07-03 08:02:50',0,NULL,0,NULL,0,'2025-07-03 08:02:50',NULL,NULL,NULL,NULL),(59,82001,NULL,'',0,1742784526673,'查询动态列表','2025-08-08 04:09:57',1564483634841,NULL,0,NULL,0,'2025-08-08 04:09:58','[\"Moment\", \"Tommy\"]','','','{}'),(1759994627546,82001,NULL,'',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2025-10-09 07:23:47',1546414192830,NULL,0,NULL,0,'2025-10-09 07:23:47','[\"Moment\"]',NULL,NULL,NULL),(1761204465097,82001,NULL,'82001',0,1714655556079,'查询动态列表-查询动态详情-查询评论列表-回复评论','2025-10-23 07:27:45',1704192205420,NULL,0,NULL,0,'2025-10-23 07:27:45','[\"Moment\"]','Test User','13000082001','{\"id\": 82001, \"name\": \"Test User\", \"phone\": \"13000082001\", \"account\": \"13000082001\", \"baseUrl\": \"http://localhost:8080\", \"password\": \"123456\", \"redCount\": 7, \"remember\": false, \"blueCount\": 13, \"greenCount\": 0, \"isLoggedIn\": true, \"totalCount\": 26, \"whiteCount\": 6, \"orangeCount\": 0, \"summaryType\": \"total\"}'),(1761204465098,82001,NULL,'',0,1742784526673,'查询动态列表','2025-10-27 03:14:19',1522905895591,NULL,0,NULL,0,'2025-10-27 03:14:20','[\"Moment\", \"Tommy\"]','','','{}'),(1761204465117,82001,NULL,'',0,1742784526673,'查询动态列表','2025-11-30 10:43:35',1511796155276,'获取验证码',0,NULL,0,'2025-11-30 10:43:35','[\"Moment\", \"Tommy\"]',NULL,NULL,NULL),(1761204465118,82001,NULL,'0',0,1765106927698,'test chain case','2025-12-07 11:28:47',0,NULL,0,NULL,0,'2025-12-07 11:28:47','[\"test\"]',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `chain` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +65,4 @@ CREATE TABLE `Chain` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-03 19:46:04
+-- Dump completed on 2025-12-07 19:38:58
--- MySQL/single/Auto/sys_Script.sql
+++ MySQL/single/Auto/sys_Script.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for macos15 (arm64)
 --
--- Host: 47.122.25.116    Database: sys
+-- Host: apijson.cn    Database: sys
 -- ------------------------------------------------------
--- Server version	5.7.43-log
+-- Server version	8.4.5
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,39 +16,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Script`
+-- Table structure for table `script`
 --
 
-DROP TABLE IF EXISTS `Script`;
+DROP TABLE IF EXISTS `script`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Script` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `userId` bigint(20) NOT NULL DEFAULT '0' COMMENT '用户 id',
-  `testAccountId` bigint(20) NOT NULL DEFAULT '0' COMMENT '测试账号 id',
-  `chainGroupId` bigint(20) NOT NULL DEFAULT '0',
-  `chainId` bigint(20) NOT NULL DEFAULT '0',
-  `documentId` bigint(20) NOT NULL DEFAULT '0' COMMENT '测试用例 id',
-  `simple` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否为可直接执行的简单代码段：0-否 1-是',
-  `ahead` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否为前置脚本',
+CREATE TABLE `script` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `userId` bigint NOT NULL DEFAULT '0' COMMENT '用户 id',
+  `testAccountId` bigint NOT NULL DEFAULT '0' COMMENT '测试账号 id',
+  `chainGroupId` bigint NOT NULL DEFAULT '0',
+  `chainId` bigint NOT NULL DEFAULT '0',
+  `documentId` bigint NOT NULL DEFAULT '0' COMMENT '测试用例 id',
+  `simple` tinyint NOT NULL DEFAULT '0' COMMENT '是否为可直接执行的简单代码段：0-否 1-是',
+  `ahead` tinyint NOT NULL DEFAULT '0' COMMENT '是否为前置脚本',
   `title` varchar(100) DEFAULT NULL COMMENT '函数名',
   `name` varchar(100) NOT NULL COMMENT '名称',
   `script` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `detail` varchar(1000) DEFAULT NULL,
+  `randomId` bigint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1680660620760 DEFAULT CHARSET=utf8 COMMENT='脚本，前置预处理脚本、后置断言和恢复脚本等';
+) ENGINE=InnoDB AUTO_INCREMENT=1738490404343 DEFAULT CHARSET=utf8mb3 COMMENT='脚本，前置预处理脚本、后置断言和恢复脚本等';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Script`
+-- Dumping data for table `script`
 --
 
-LOCK TABLES `Script` WRITE;
-/*!40000 ALTER TABLE `Script` DISABLE KEYS */;
-INSERT INTO `Script` VALUES (1,0,0,0,0,0,0,0,NULL,'getType','function getType(curObj, key) {\n    var val = curObj == null ? null : curObj[key];\n    return val instanceof Array ? \"array\" : typeof val;\n}','2022-11-16 16:01:23',NULL),(2,0,0,0,0,0,0,0,NULL,'isContain','function isContain(curObj, arrKey, valKey) {\n    var arr = curObj == null ? null : curObj[arrKey];\n    var val = curObj == null ? null : curObj[valKey];\n    return arr != null && arr.indexOf(val) >=0;\n}','2022-11-16 16:02:48',NULL),(3,0,0,0,0,0,1,0,NULL,'init','var i = 1;\n\"init done \"  + i;','2022-11-16 16:41:35',NULL),(4,0,0,0,0,0,0,0,NULL,'length','function length(curObj, key) {\n    var val = curObj == null ? null : curObj[key];\n    return val == null ? 0 : val.length;\n}','2022-11-16 17:18:43',NULL),(1670877914051,82001,0,0,0,0,1,1,'执行脚本 2022-12-13 04:44','','function assert(assertion, msg) {\n     if (assertion === true) {\n         return\n     }\n     if (msg == null) {\n         msg = \'assert failed! assertion = \' + assertion\n     }\n\n     if (isTest) {\n         console.log(msg)\n         alert(msg)\n     } else {\n         throw new Error(msg)\n     } \n}  \n\nif (isTest) {\n     assert(true)\n     assert(false)\n     assert(true, \'ok\')\n     assert(false, \'data.User shoule not be null!\') \n}\n\nfunction getCurAccount() {\n  return App.getCurrentAccount()\n}','2022-12-12 20:45:14',NULL),(1670878495619,82001,82002,0,0,0,1,0,'执行脚本 2022-12-13 04:54','','function getCurAccount() {\n  return App.getCurrentAccount()\n}','2022-12-12 20:54:55',NULL),(1670878529042,82001,82001,0,0,0,1,1,'执行脚本 2022-12-13 04:55','','function getCurAccount() {\n  return App.getCurrentAccount()\n}','2022-12-12 20:55:29',NULL),(1670878622401,82001,82003,0,0,0,1,0,'执行脚本 2022-12-13 04:57','','if (isPre) {\n  header[\'my-header\'] = \'test\'\n}','2022-12-12 20:57:02',NULL),(1670885503909,82001,0,0,0,1657045372046,1,1,'执行脚本 2022-12-13 06:51','','if (isPre) {\n  req.User.id = 82005\n}','2022-12-12 22:51:43',NULL),(1670887211207,82001,0,0,0,1657045372046,1,0,'执行脚本 2022-12-13 07:20','','','2022-12-12 23:20:11',NULL),(1676368454070,82001,0,0,0,1546414192830,1,0,NULL,'casePost1546414192830','','2023-02-14 09:54:14',NULL),(1679282174670,82001,0,0,0,0,1,1,NULL,'到店系统查询','','2023-03-20 03:16:14',NULL),(1680660620759,82001,0,0,0,1511796155276,1,1,NULL,'casePre1511796155276','if (isPre) {\n    console.log(\'test pre script 4 /post/verify\')    \n}\n','2024-05-03 03:55:14',NULL);
-/*!40000 ALTER TABLE `Script` ENABLE KEYS */;
+LOCK TABLES `script` WRITE;
+/*!40000 ALTER TABLE `script` DISABLE KEYS */;
+INSERT INTO `script` VALUES (1,0,0,0,0,0,0,0,NULL,'getType','function getType(curObj, key) {\n    var val = curObj == null ? null : curObj[key];\n    return val instanceof Array ? \"array\" : typeof val;\n}','2022-11-16 16:01:23',NULL,0),(2,0,0,0,0,0,0,0,NULL,'isContain','function isContain(curObj, arrKey, valKey) {\n    var arr = curObj == null ? null : curObj[arrKey];\n    var val = curObj == null ? null : curObj[valKey];\n    return arr != null && arr.indexOf(val) >=0;\n}','2022-11-16 16:02:48',NULL,0),(3,0,0,0,0,0,1,0,NULL,'init','var i = 1;\n\"init done \"  + i;','2022-11-16 16:41:35',NULL,0),(4,0,0,0,0,0,0,0,NULL,'length','function length(curObj, key) {\n    var val = curObj == null ? null : curObj[key];\n    return val == null ? 0 : val.length;\n}','2022-11-16 17:18:43',NULL,0),(1670877914051,82001,0,0,0,0,1,1,'执行脚本 2022-12-13 04:44','','function assert(assertion, msg) {\n     if (assertion === true) {\n         return\n     }\n     if (msg == null) {\n         msg = \'assert failed! assertion = \' + assertion\n     }\n\n     if (isTest) {\n         console.log(msg)\n         alert(msg)\n     } else {\n         throw new Error(msg)\n     } \n}  \n\nif (isTest) {\n     assert(true)\n     assert(false)\n     assert(true, \'ok\')\n     assert(false, \'data.User shoule not be null!\') \n}\n\nfunction getCurAccount() {\n  return App.getCurrentAccount()\n}','2022-12-12 20:45:14',NULL,0),(1670878495619,82001,82002,0,0,0,1,0,'执行脚本 2022-12-13 04:54','','function getCurAccount() {\n  return App.getCurrentAccount()\n}','2022-12-12 20:54:55',NULL,0),(1670878529042,82001,82001,0,0,0,1,1,'执行脚本 2022-12-13 04:55','','function getCurAccount() {\n  return App.getCurrentAccount()\n}','2022-12-12 20:55:29',NULL,0),(1670878622401,82001,82003,0,0,0,1,0,'执行脚本 2022-12-13 04:57','','if (isPre) {\n  header[\'my-header\'] = \'test\'\n}','2022-12-12 20:57:02',NULL,0),(1670885503909,82001,0,0,0,1657045372046,1,1,'执行脚本 2022-12-13 06:51','','if (isPre) {\n  req.User.id = 82005\n}','2022-12-12 22:51:43',NULL,0),(1670887211207,82001,0,0,0,1657045372046,1,0,'执行脚本 2022-12-13 07:20','','','2022-12-12 23:20:11',NULL,0),(1676368454070,82001,0,0,0,1546414192830,1,0,NULL,'casePost1546414192830','','2023-02-14 09:54:14',NULL,0),(1679282174670,82001,0,0,0,0,1,1,NULL,'到店系统查询','','2023-03-20 03:16:14',NULL,0),(1680660620759,82001,0,0,0,1511796155276,1,1,NULL,'casePre1511796155276','if (isPre) {\n    console.log(\'test pre script 4 /post/verify\')    \n}\n','2024-05-03 03:55:14',NULL,0),(1680660620760,82001,0,1716706529846,42,1511970009072,1,1,NULL,'casePre1511970009072','if (isPre) { console.log(\"test pre script\") }','2024-09-08 16:00:15',NULL,0),(1680660620761,82001,0,1716706529846,42,1511970009072,1,0,NULL,'casePost1511970009072','console.log(\'run post script\')','2024-09-08 16:00:32',NULL,0),(1680660620762,82001,0,1714209723275,2,1546414192830,1,1,NULL,'casePre1546414192830','','2024-11-28 02:10:39',NULL,0),(1738490311550,0,0,0,0,1560244940013,1,0,NULL,'casePost1560244940013','','2025-02-02 09:58:32',NULL,0),(1738490327734,0,0,0,0,0,1,0,NULL,'globalPost0','if (data != null) {\n    delete data[\"sql:generate|cache|execute|maxExecute\"]\n}\n','2025-02-02 09:58:48',NULL,0),(1738490404341,0,0,0,0,0,1,1,NULL,'globalPre0','req[\'@explain\'] = true','2025-02-02 10:00:04',NULL,0),(1738490404342,82001,0,0,0,0,1,1,NULL,'','','2025-03-11 08:54:24',NULL,0);
+/*!40000 ALTER TABLE `script` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -60,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-16  0:37:30
+-- Dump completed on 2025-12-07 19:38:59
